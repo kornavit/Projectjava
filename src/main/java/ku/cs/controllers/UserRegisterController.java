f11caa450351e643173a779a70e3ea3670355cbf
Joe Sornsiri <sornsiri.h@ku.th>
2022-09-07 00:23:12 +0700

    update register_changepassword button & add user_changepassword view&controller

--- src/main/java/ku/cs/controllers/UserRegisterController.java
+++ src/main/java/ku/cs/controllers/UserRegisterController.java
@@ -4,24 +4,44 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import ku.cs.models.modelRegister;
+import ku.cs.services.ImageDataSource;
 
 import java.io.IOException;
 
+
 public class NisitRegisterController {
+    private modelRegister selectedPhoto;
+
+
     @FXML private TextField nameTextField;
     @FXML private TextField passTextField;
     @FXML private TextField cpTextField;
     @FXML private TextField userTextField;
     @FXML private Label resultCheckUsername;
+    @FXML private ImageView nisitPhoto;
+
+    private ImageDataSource getImage;
+
+
 
     @FXML public void initialize(){
+        nisitPhoto.setImage(new Image(getClass().getResource("/ku/cs/images/default-profile.jpg").toExternalForm()));
     }
+
     public void handleCheckUsernameButton(ActionEvent actionEvent) {
+
     }
 
     public void handleUploadNisitPhotoButton(ActionEvent actionEvent) {
-    }
+        getImage = new ImageDataSource();
+        String pic_target = getImage.chooseImage("user_images");
+        nisitPhoto.setImage(new Image(pic_target));
 
+    }
     public void handleSubmitButton(ActionEvent actionEvent) {
         try {
             com.github.saacsos.FXRouter.goTo("success");
@@ -30,6 +50,8 @@ public class NisitRegisterController {
             System.err.println("ให้ตรวจสอบการกำหนด route");
         }
     }
+
+
 
     public void handleBackNisitRegisterButton(ActionEvent actionEvent) {
         try {
